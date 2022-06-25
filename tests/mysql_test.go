c7f1f13bd74eec98a3796874838caa25a32c1e70
wellington gadelha <contato.informeai@gmail.com>
2022-06-25 10:09:22 -0300

    feat(tests/mysql_test.go): create file for tests mysql

---
+++ tests/mysql_test.go
@@ -0,0 +1,28 @@
+package tests
+
+import (
+	"log"
+	"testing"
+
+	"github.com/informeai/gedb/libs"
+)
+
+//go test -run TestMysqlConnector -v
+func TestNewMysqlConnector(t *testing.T) {
+	mysql_connector := libs.NewMysqlConnector("gedb", "root", "", "127.0.0.1", "3306")
+	if mysql_connector == nil {
+		t.Errorf("TestNewMysqlConnector: expect != nil, got: nil\n")
+	}
+}
+
+//go test -run TestMysqlConnect -v
+func TestMysqlConnect(t *testing.T) {
+	mysql_connector := libs.NewMysqlConnector("gedb", "root", "", "127.0.0.1", "3306")
+	err := mysql_connector.Connect()
+	if err != nil {
+		t.Errorf("TestMysqlConnect: expect not error, got: %s\n", err.Error())
+	}
+	log.Printf("Connection of mysql: %v\n", mysql_connector)
+	mysql_connector.Close()
+
+}
