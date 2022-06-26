874cc4a07a3f54f3b9d83ad3862afcc9c8254adf
wellington gadelha <contato.informeai@gmail.com>
2022-06-26 09:13:07 -0300

    refac(tests/mysql_test.go): add implementation for tests the export and list tables

--- tests/mysql_test.go
+++ tests/mysql_test.go
@@ -26,3 +26,33 @@ func TestMysqlConnect(t *testing.T) {
 	mysql_connector.Close()
 
 }
+
+//go test -run TestMysqlListTables
+func TestMysqlListTables(t *testing.T) {
+	mysql_connector := libs.NewMysqlConnector("gedb", "root", "", "127.0.0.1", "3306")
+	err := mysql_connector.Connect()
+	if err != nil {
+		t.Errorf("TestMysqlListTables: expect: != nil, got: %s\n", err.Error())
+	}
+	err = mysql_connector.ListTables()
+	if err != nil {
+		t.Errorf("TestMysqlListTables: expect: != nil, got: %s\n", err.Error())
+	}
+	mysql_connector.Close()
+}
+
+//go test -run TestMysqlListTables
+func TestMysqlExport(t *testing.T) {
+	mysql_connector := libs.NewMysqlConnector("gedb", "root", "", "127.0.0.1", "3306")
+	err := mysql_connector.Connect()
+	if err != nil {
+		t.Errorf("TestMysqlExport: expect: != nil, got: %s\n", err.Error())
+	}
+	for _, value := range []string{"json", "csv"} {
+		err = mysql_connector.Export(value)
+		if err != nil {
+			t.Errorf("TestMysqlExport: expect: != nil, got: %s\n", err.Error())
+		}
+	}
+	mysql_connector.Close()
+}
