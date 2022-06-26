7516b010e9bc3587412f785274d27dfe223200c9
wellington gadelha <contato.informeai@gmail.com>
2022-06-26 09:12:19 -0300

    refac(libs/mysql.go): add implementation for parse the formats json and csv

--- libs/mysql.go
+++ libs/mysql.go
@@ -1,15 +1,21 @@
 package libs
 
 import (
+	"context"
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/sqltocsv"
 )
 
 //MysqlConnector is struct for connect in db
 type MysqlConnector struct {
 	db       *sql.DB
+	tables   []string
 	Username string
 	Password string
 	Host     string
@@ -37,6 +43,165 @@ func (m *MysqlConnector) Connect() (err error) {
 	m.db = db
 	err = nil
 	return
+}
+
+//getTables return tables name for connector
+func (m *MysqlConnector) getTables() (err error) {
+	rows, err := m.db.QueryContext(context.Background(), "SHOW TABLES")
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		var table_name string
+		if err := rows.Scan(&table_name); err != nil {
+			log.Fatal(err)
+		}
+		m.tables = append(m.tables, table_name)
+	}
+	err = nil
+	return
+}
+
+//getJSON return data of table
+func (m *MysqlConnector) getJSON(sql_string string) (string, error) {
+	stmt, err := m.db.Prepare(sql_string)
+	if err != nil {
+		return "", err
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query()
+	if err != nil {
+		return "", err
+	}
+	defer rows.Close()
+
+	columns, err := rows.Columns()
+	if err != nil {
+		return "", err
+	}
+
+	tableData := make([]map[string]interface{}, 0)
+
+	count := len(columns)
+	values := make([]interface{}, count)
+	scanArgs := make([]interface{}, count)
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	for rows.Next() {
+		err := rows.Scan(scanArgs...)
+		if err != nil {
+			return "", err
+		}
+
+		entry := make(map[string]interface{})
+		for i, col := range columns {
+			v := values[i]
+
+			b, ok := v.([]byte)
+			if ok {
+				entry[col] = string(b)
+			} else {
+				entry[col] = v
+			}
+		}
+
+		tableData = append(tableData, entry)
+	}
+
+	jsonData, err := json.Marshal(tableData)
+	if err != nil {
+		return "", err
+	}
+
+	return string(jsonData), nil
+}
+
+//getCSV return data of table in csv format
+func (m *MysqlConnector) getCSV(sql_string string) (string, error) {
+	stmt, err := m.db.Prepare(sql_string)
+	if err != nil {
+		return "", err
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query()
+	if err != nil {
+		return "", err
+	}
+	defer rows.Close()
+	converter := sqltocsv.New(rows)
+	return converter.String(), nil
+}
+
+//ListTables return all tables in database
+func (m *MysqlConnector) ListTables() (err error) {
+	if err = m.getTables(); err != nil {
+		return err
+	}
+	for _, table := range m.tables {
+		fmt.Printf("%s\n", table)
+	}
+	return nil
+}
+
+//Export save data of tables in files
+func (m *MysqlConnector) Export(format string) (err error) {
+	if err = m.getTables(); err != nil {
+		return err
+	}
+	switch format {
+	case "json":
+		for _, table := range m.tables {
+			f, err := os.Create(fmt.Sprintf("./%s.%s", table, format))
+			if err != nil {
+				return err
+			}
+			json_data, err := m.getJSON(fmt.Sprintf("SELECT * FROM %s", table))
+			if err != nil {
+				return err
+			}
+			bytes_content := []byte(json_data)
+			if _, err = f.Write(bytes_content); err != nil {
+				return err
+			}
+			if err = f.Sync(); err != nil {
+				return err
+			}
+			if err = f.Close(); err != nil {
+				return err
+			}
+		}
+		break
+	case "csv":
+		for _, table := range m.tables {
+			f, err := os.Create(fmt.Sprintf("./%s.%s", table, format))
+			if err != nil {
+				return err
+			}
+			csv_data, err := m.getCSV(fmt.Sprintf("SELECT * FROM %s", table))
+			if err != nil {
+				return err
+			}
+			bytes_content := []byte(csv_data)
+			if _, err = f.Write(bytes_content); err != nil {
+				return err
+			}
+			if err = f.Sync(); err != nil {
+				return err
+			}
+			if err = f.Close(); err != nil {
+				return err
+			}
+		}
+	default:
+		fmt.Printf("Error: Format %s not accepted\n", format)
+		os.Exit(1)
+	}
+
+	return nil
 }
 
 //Connect is connect in db
