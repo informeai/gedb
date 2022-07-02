0d186fa62e9dcb01654a75b3a11cf208c8af888e
wellington gadelha <contato.informeai@gmail.com>
2022-07-02 18:39:40 -0300

    refac(services/export_services.go): add implementation for export

--- services/export_services.go
+++ services/export_services.go
@@ -1,5 +1,12 @@
 package services
 
+import (
+	"errors"
+
+	"github.com/informeai/gedb/helpers"
+	"github.com/informeai/gedb/libs"
+)
+
 //ExportService is struct for export data from driver.
 type ExportService struct {
 	Args    []string
@@ -15,5 +22,24 @@ func NewExportService() *ExportService {
 
 //Export execute creation of files of data the database.
 func (e *ExportService) Export() error {
+	if err := helpers.VerifyFlags(e.Flags); err != nil {
+		return err
+	}
+	switch e.Driver {
+	case "mysql":
+		connector := libs.NewMysqlConnector(e.Flags["-db"], e.Flags["-user"], e.Flags["-pass"], e.Flags["-host"], "3306")
+		if err := connector.Connect(); err != nil {
+			return err
+		}
+		if err := connector.Export(e.Flags["-format"]); err != nil {
+			return err
+		}
+		if err := connector.Close(); err != nil {
+			return err
+		}
+		break
+	default:
+		return errors.New("driver not permited")
+	}
 	return nil
 }
