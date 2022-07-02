dae8ba6c5116770e348c2e851ebad480c071d968
wellington gadelha <contato.informeai@gmail.com>
2022-07-02 18:09:43 -0300

    feat(helpers/verify_flags.go): create file for verify flags

---
+++ helpers/verify_flags.go
@@ -0,0 +1,47 @@
+package helpers
+
+import (
+	"errors"
+	"fmt"
+)
+
+//flagsUtility is slice of flags the utility
+var flagsUtility = []string{"-host", "-user", "-pass", "-db", "-format"}
+
+//VerifyFlags execute verification for flags
+func VerifyFlags(flags map[string]string) error {
+	for key, value := range flags {
+		if err := contains(flagsUtility, key); err != nil {
+			return err
+		}
+		if err := empty(key, value); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//contains verify if flag exist.
+func contains(flags []string, name string) error {
+	existed := false
+	var flg string
+	for _, flag := range flags {
+		if flag == name {
+			existed = true
+			flg = flag
+			break
+		}
+	}
+	if existed == false {
+		return errors.New(fmt.Sprintf("flag: [%s] not permited", flg))
+	}
+	return nil
+}
+
+//empty verify if flag is empty.
+func empty(flag string, value string) error {
+	if len(value) == 0 {
+		return errors.New(fmt.Sprintf("value: [%s] the flag -> [%s] not permited\n", value, flag))
+	}
+	return nil
+}
