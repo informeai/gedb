e09c3be84bff883f87b162c942a2b7b73348c47f
wellington gadelha <contato.informeai@gmail.com>
2022-07-02 18:37:54 -0300

    chore(tests/verify_flags_test.go): update logic of verify flags test

--- tests/verify_flags_test.go
+++ tests/verify_flags_test.go
@@ -13,16 +13,18 @@ import (
 //go test -run -v TestVerifyFlags
 func TestVerifyFlags(t *testing.T) {
 	hostFlag := flag.String("host", "127.0.0.1", "host of connection")
-	databaseFlag := flag.String("db", "", "database of connection")
-	usernameFlag := flag.String("user", "", "username of connection")
-	passwordFlag := flag.String("pass", "", "password of connection")
+	databaseFlag := flag.String("db", "gedb", "database of connection")
+	usernameFlag := flag.String("user", "admin", "username of connection")
+	passwordFlag := flag.String("pass", "secret", "password of connection")
 	formatFlag := flag.String("format", "json", "format to export")
+	portFlag := flag.String("P", "3306", "port to connection")
 	flags := map[string]string{
 		"-host":   *hostFlag,
 		"-db":     *databaseFlag,
 		"-user":   *usernameFlag,
 		"-pass":   *passwordFlag,
 		"-format": *formatFlag,
+		"-P":      *portFlag,
 	}
 	fmt.Printf("flags: %+v\n", flag.Args())
 	flag.Parse()
