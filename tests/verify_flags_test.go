21be52c48952ff44bd5432332385776b491e1c02
wellington gadelha <contato.informeai@gmail.com>
2022-07-02 18:10:26 -0300

    feat(tests/verify_flags_test.go): create file for test in verify flags

---
+++ tests/verify_flags_test.go
@@ -0,0 +1,35 @@
+package tests
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/informeai/gedb/helpers"
+	"github.com/informeai/gedb/templates"
+)
+
+//go test -run -v TestVerifyFlags
+func TestVerifyFlags(t *testing.T) {
+	hostFlag := flag.String("host", "127.0.0.1", "host of connection")
+	databaseFlag := flag.String("db", "", "database of connection")
+	usernameFlag := flag.String("user", "", "username of connection")
+	passwordFlag := flag.String("pass", "", "password of connection")
+	formatFlag := flag.String("format", "json", "format to export")
+	flags := map[string]string{
+		"-host":   *hostFlag,
+		"-db":     *databaseFlag,
+		"-user":   *usernameFlag,
+		"-pass":   *passwordFlag,
+		"-format": *formatFlag,
+	}
+	fmt.Printf("flags: %+v\n", flag.Args())
+	flag.Parse()
+	err := helpers.VerifyFlags(flags)
+	if err != nil {
+		t.Errorf("TestVerifyFlags: expect: nil, got: %s\n", err.Error())
+		log.Printf("%s\n", templates.Header)
+		log.Printf("%s\n", templates.Usage)
+	}
+}
